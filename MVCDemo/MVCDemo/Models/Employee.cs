c9edad8014b06c5c9fc94233a9012a62f0a899c3
Ramesh Majji <majjiramesh143@gmail.com>
2022-11-08 17:08:14 +0530

    Employee Model fields added

--- MVCDemo/MVCDemo/Models/Employee.cs
+++ MVCDemo/MVCDemo/Models/Employee.cs
@@ -7,5 +7,9 @@ namespace MVCDemo.Models
 {
     public class Employee
     {
+        public int EmployeeId { get; set; }
+        public string EmpName { get; set; }
+        public string Gender { get; set; }
+        public string city { get; set; }
     }
 }
